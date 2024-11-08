13fe870bc3e38d5bece95b05b189888325093526
Bobby1 <bwen@yahoo.com>
2024-11-07 16:04:51 -0800

    Initial deploy

---
+++ 03-externaldns-install-terraform-manifests/c2-remote-state-datasource.tf
@@ -0,0 +1,10 @@
+# Terraform Remote State Datasource - Remote Backend AWS S3
+data "terraform_remote_state" "eks" {
+  backend = "s3"
+  config = {
+    bucket = "b1dev-terraform-state-eks"
+    # key    = "dev/eks-cluster/terraform.tfstate"
+    key    = "dev/eks-cluster/43-terraform.tfstate"
+    region = var.aws_region
+  }
+}
---
+++ 06-run-on-fargate-terraform-manifests/c2-remote-state-datasource.tf
@@ -0,0 +1,9 @@
+# Terraform Remote State Datasource - Remote Backend AWS S3
+data "terraform_remote_state" "eks" {
+  backend = "s3"
+  config = {
+    bucket = "b1dev-terraform-state-eks"
+    key    = "dev/eks-cluster/43-terraform.tfstate"
+    region = "us-east-1"
+  }
+}
